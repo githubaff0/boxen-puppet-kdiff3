fe493d87120ad0211f7eff7aec15b995cbe69015
aff0 <git.aff0@spamgourmet.com>
2013-03-23 21:50:44 -0400

    Initial boxen-puppet-kdiff3 commit (original baseline repo copied from git@github.com:boxen/puppet-repository.git)

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# Public: Install kdiff3 via homebrew
+#
+# Examples
+#
+#   include kdiff3
+class kdiff3 {
+  package { 'kdiff3':
+    ensure   => installed,
+    source   => 'http://sourceforge.net/projects/kdiff3/files/kdiff3/0.9.97/kdiff3_0.9.97_MacOS_64bit.dmg/download',
+    provider => appdmg,
+  }
+}
